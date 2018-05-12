29eafcac5a866e821dcc512ba699cbe7b5092e70
Felipe Lamim <fclamim@gmail.com>
2018-05-12 13:08:29 -0300

    implementando as 4 operações

--- controller/ct_calculadora.php
+++ controller/ct_calculadora.php
@@ -11,6 +11,18 @@ include_once '../model/calculadora.class.php';
         if (isset($_POST['somar'])){ 
              echo $calc->Somar(n1, n2);
         }
+
+        if (isset($_POST['subtrair'])){
+            echo $calc->Subtrair(n1, n2);
+        }
+
+        if(isset($_POST['multiplicar'])){
+            echo $calc->Multiplicacao(n1, n2);
+        }
+
+        if(isset($_POST['dividir'])){
+            echo $calc->Dividir(n1, n2);
+        }
                     
     }
 
